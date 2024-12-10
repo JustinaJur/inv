1aeedb3c8e173add3195cee5c38f70c3dcf9f213
JustinaJur <jurgeleviciute.justina@gmail.com>
2024-12-10 22:50:31 +0200

    update b docker

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903c1fc225588403210c560e0c91d1690d76a0d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d924829acc7d8a10cbe28b8a748364313fdc87")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
